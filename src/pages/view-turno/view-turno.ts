b610e9ef28f91a7ceebf8f13181dc7b2bc077d74
dom061077 <dom061077@gmail.com>
2017-11-17 16:35:28 -0300

    hosting configuracion

--- src/pages/view-turno/view-turno.ts
+++ src/pages/view-turno/view-turno.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TurnoItem  } from '../../models/turnos/turno-item.interface';
+
+
+//https://medium.com/google-developer-experts/deploy-your-app-to-firebase-in-seconds-b3a9a37dff47
 
 /**
  * Generated class for the ViewTurnoPage page.
@@ -14,6 +18,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'view-turno.html',
 })
 export class ViewTurnoPage {
+
+  turnoItem : TurnoItem = {$key:'',paciente:{},start:'',end:'',title:''};
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
