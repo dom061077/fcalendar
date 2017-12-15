b71091102bbca1bde1181428ae7fad48485a5562
dom061077 <dom061077@gmail.com>
2017-12-15 17:27:56 -0300

    change password

--- src/pages/change-password/change-password.ts
+++ src/pages/change-password/change-password.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { UsuariosServiceProvider } from '../../providers/usuarios-service/usuarios-service';
 import { FormGroup, FormBuilder, FormControl, Validators,ReactiveFormsModule  } from "@angular/forms";
 import { UserValidator } from '../../validators/user.validator';
+import { AlertController } from 'ionic-angular';
+import { HomePage } from '../../pages/home/home';
 
 /**
  * Generated class for the ChangePasswordPage page.
@@ -23,20 +25,35 @@ export class ChangePasswordPage {
   constructor(public navCtrl: NavController, public navParams: NavParams
               ,private userService:UsuariosServiceProvider
               ,private formBuilder:FormBuilder
-              ,private userValidator:UserValidator) {
+              ,private userValidator:UserValidator
+              ,private alertCtrl:AlertController) {
 
   }
 
   confirmar(){
-     this.userService.changePassword(this.password).catch(e=>{
-          console.log('Error: '+e);
+          //"auth/weak-password"
+          //"auth/email-already-in-use"    
+     this.userService.changePassword(this.password).then(()=>{
+            this.navCtrl.setRoot(HomePage);
+        }).catch(e=>{
+          var mensaje='';
+          if(e.code=='auth/weak-password')
+              mensaje = 'La contraseña debe tener al menos 6 caracteres';
+          
+          console.log('Excepción: '+e.code);
+          let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: mensaje,
+            buttons: ['OK']
+          });
+          alert.present();
      });
   } 
 
   ngOnInit(){
       this.formPass = this.formBuilder.group({
-        'password' : ['',[Validators.required],this.userValidator.passwordMatch.bind(this.userValidator)],
-        'password2' : ['',[Validators.required],this.userValidator.password2Match.bind(this.userValidator)]
+      'password' : ['',[Validators.required]/*,this.userValidator.passwordMatch.bind(this.userValidator)*/],
+      'password2' : ['',[Validators.required],this.userValidator.password2Match.bind(this.userValidator)]
       });
 
 
